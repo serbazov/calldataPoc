6c6bd5be581e17665a4e4432341566c01fc664d6
Sergey Bazov <s.bazov@entangle.fi>
2024-12-25 18:51:43 +0300

    add explicit method for comparison

--- contracts/DataProcessor.sol
+++ contracts/DataProcessor.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "hardhat/console.sol";
+import "./UnsafeCalldataBytesLib.sol";
 
 contract DataProcessor {
     bytes32 public storedData;
@@ -13,6 +15,14 @@ contract DataProcessor {
         assembly {
             messageEncoded := calldataload(payloadOffset)
         }
+        storedData = messageEncoded;
+    }
+    function processDataExplicit(bytes calldata _calldata) external {
+        uint256 _timestamp = UnsafeCalldataBytesLib.toUint256(_calldata, 0);
+        bytes32 messageEncoded = UnsafeCalldataBytesLib.toBytes32(
+            _calldata,
+            32
+        );
         storedData = messageEncoded;
     }
 }
--- test/DataProcessor.ts
+++ test/DataProcessor.ts
@@ -16,11 +16,31 @@ describe("DataProcessor", function () {
     ]);
     let signers = await ethers.getSigners();
     console.log(payload); // 0xdd0e34ef00000000000000000000000000000000000000000000000000000000676c19a668656c6c6f576f726c6400000000000000000000000000000000000000000000
-    await signers[0].sendTransaction({
+    let tx = await signers[0].sendTransaction({
       to: dataProcessor!,
       data: payload,
     });
+    let rec = await tx.wait();
+    console.log("gasUsed", rec!.gasUsed);
+    expect(await dataProcessor.storedData()).to.equal(helloWorld);
+  });
+  it("should process data Explicit and store helloWorld in storage", async () => {
+    const dataProcessor = await ethers.deployContract("DataProcessor");
+    const timestamp = Math.floor(Date.now() / 1000);
+    const helloWorld = ethers.encodeBytes32String("helloWorld");
 
+    const payload = ethers.concat([
+      ethers.AbiCoder.defaultAbiCoder().encode(["uint256"], [timestamp]),
+      helloWorld,
+    ]);
+    let signers = await ethers.getSigners();
+    console.log(payload); // 0x00000000000000000000000000000000000000000000000000000000676c287768656c6c6f576f726c6400000000000000000000000000000000000000000000
+
+    let tx = await dataProcessor
+      .connect(signers[0])
+      .processDataExplicit(payload);
+    let rec = await tx.wait();
+    console.log("gasUsed", rec!.gasUsed);
     expect(await dataProcessor.storedData()).to.equal(helloWorld);
   });
 });
